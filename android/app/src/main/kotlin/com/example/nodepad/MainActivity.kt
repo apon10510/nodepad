183b9426f16edf501df74e8b5e787a66da23296a
apon <mdaponahmed412@gmail.com>
2024-05-21 23:32:33 +0600

    nodepad

---
+++ android/app/src/main/kotlin/com/example/nodepad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nodepad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,138 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_quill/flutter_quill.dart';
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   QuillController controller = QuillController.basic();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           QuillToolbar.simple(
+//             configurations: QuillSimpleToolbarConfigurations(
+//               controller: controller,
+//               sharedConfigurations: const QuillSharedConfigurations(
+//                 locale: Locale('de'),
+//               ),
+//             ),
+//           ),
+//           Expanded(
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: Card(
+//                 color: Colors.white,
+//                 child: QuillEditor.basic(
+//                   configurations: QuillEditorConfigurations(
+//                     controller: controller,
+//                     // readOnly: false,
+//                     sharedConfigurations: const QuillSharedConfigurations(
+//                       locale: Locale('de'),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late QuillController controller;
+  late SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedData();
+  }
+
+  Future<void> _loadSavedData() async {
+    prefs = await SharedPreferences.getInstance();
+    String? savedData = prefs.getString('editorData');
+    if (savedData != null) {
+      var myJSON = jsonDecode(savedData);
+      controller = QuillController(
+        document: Document.fromJson(myJSON),
+        selection: const TextSelection.collapsed(offset: 0),
+      );
+    } else {
+      controller = QuillController.basic();
+    }
+    setState(() {
+      controller.addListener(_saveData);
+    });
+  }
+
+  void _saveData() {
+    String json = jsonEncode(controller.document.toDelta().toJson());
+    prefs.setString('editorData', json);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_saveData);
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null) {
+      return const CircularProgressIndicator(); // Show a loading indicator while initializing
+    }
+
+    return Scaffold(
+      body: Column(
+        children: [
+          QuillToolbar.simple(
+            configurations: QuillSimpleToolbarConfigurations(
+              controller: controller,
+              sharedConfigurations: const QuillSharedConfigurations(
+                locale: Locale('de'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Card(
+                child: QuillEditor.basic(
+                  configurations: QuillEditorConfigurations(
+                    padding: const EdgeInsets.all(8),
+                    controller: controller,
+                    // readOnly: false,
+                    sharedConfigurations: const QuillSharedConfigurations(
+                      locale: Locale('de'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
